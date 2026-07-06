6b895b79b3f85d22b5b2a42bf58212949e0e5962
koh1o <243413435+koh1o@users.noreply.github.com>
2026-07-06 13:06:36 +0300

    Initialize Spring Boot project with hello endpoint

---
+++ src/main/java/io/github/koh1o/internshiptrackerapi/InternshipTrackerApiApplication.java
@@ -0,0 +1,13 @@
+package io.github.koh1o.internshiptrackerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternshipTrackerApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InternshipTrackerApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/koh1o/internshiptrackerapi/InternshipTrackerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.koh1o.internshiptrackerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternshipTrackerApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
