6c337e37c3418a0bd21e75c3885123ab5df2f149
koh1o <243413435+koh1o@users.noreply.github.com>
2026-07-08 17:02:11 +0300

    Add company lookup endpoint

--- src/main/java/io/github/koh1o/internshiptrackerapi/controller/CompanyController.java
+++ src/main/java/io/github/koh1o/internshiptrackerapi/controller/CompanyController.java
@@ -5,12 +5,14 @@ import io.github.koh1o.internshiptrackerapi.service.CompanyService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/companies")
@@ -34,5 +36,17 @@ public class CompanyController {
         return ResponseEntity
                 .status(HttpStatus.CREATED)
                 .body(savedCompany);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Company> getCompanyById(
+            @PathVariable Long id
+    ) {
+        Optional<Company> companyOptional = companyService.getCompanyById(id);
+        if (companyOptional.isPresent()) {
+            return ResponseEntity.ok(companyOptional.get());
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 }
