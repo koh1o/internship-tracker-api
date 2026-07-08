a07fe4c8b5e262cb99bdb5eb42ace19d8d45a942
koh1o <243413435+koh1o@users.noreply.github.com>
2026-07-08 14:23:35 +0300

    Add company creation endpoint

--- src/main/java/io/github/koh1o/internshiptrackerapi/controller/CompanyController.java
+++ src/main/java/io/github/koh1o/internshiptrackerapi/controller/CompanyController.java
@@ -2,7 +2,11 @@ package io.github.koh1o.internshiptrackerapi.controller;
 
 import io.github.koh1o.internshiptrackerapi.entity.Company;
 import io.github.koh1o.internshiptrackerapi.service.CompanyService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,5 +24,15 @@ public class CompanyController {
     @GetMapping
     public List<Company> getAllCompanies() {
         return companyService.getAllCompanies();
+    }
+
+    @PostMapping
+    public ResponseEntity<Company> createCompany(
+            @RequestBody Company company
+    ) {
+        Company savedCompany = companyService.createCompany(company);
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(savedCompany);
     }
 }
