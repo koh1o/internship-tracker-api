6a3838c8b3b29cc5621195407279d645d7639ced
koh1o <243413435+koh1o@users.noreply.github.com>
2026-07-09 12:48:37 +0300

    Add company update endpoint

--- src/main/java/io/github/koh1o/internshiptrackerapi/controller/CompanyController.java
+++ src/main/java/io/github/koh1o/internshiptrackerapi/controller/CompanyController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -57,5 +58,17 @@ public class CompanyController {
     ) {
         companyService.deleteCompany(id);
         return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Company> updateCompany(
+            @PathVariable Long id,
+            @RequestBody Company company
+    ) {
+        Optional<Company> updatedCompanyOptional = companyService.updateCompany(id, company);
+        if (updatedCompanyOptional.isPresent()) {
+            return ResponseEntity.ok(updatedCompanyOptional.get());
+        }
+        return ResponseEntity.notFound().build();
     }
 }
