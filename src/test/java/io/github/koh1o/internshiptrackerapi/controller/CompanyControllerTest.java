514a014f80405e91e51d0a28e8f3341f2a421768
koh1o <243413435+koh1o@users.noreply.github.com>
2026-07-09 13:59:53 +0300

    Add company creation controller test

--- src/test/java/io/github/koh1o/internshiptrackerapi/controller/CompanyControllerTest.java
+++ src/test/java/io/github/koh1o/internshiptrackerapi/controller/CompanyControllerTest.java
@@ -5,15 +5,18 @@ import io.github.koh1o.internshiptrackerapi.service.CompanyService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.List;
 import java.util.Optional;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -60,5 +63,31 @@ class CompanyControllerTest {
         mockMvc.perform(get("/api/companies/{id}", companyId))
                 .andExpect(status().isNotFound());
         verify(companyService).getCompanyById(companyId);
+    }
+
+    @Test
+    void shouldCreateCompany() throws Exception {
+        String requestBody = """
+        {
+          "name": "JetBrains",
+          "website": "https://www.jetbrains.com",
+          "description": "Software development company"
+        }
+        """;
+
+        Company createdCompany = new Company(
+                "JetBrains",
+                "https://www.jetbrains.com",
+                "Software development company"
+        );
+
+        when(companyService.createCompany(any(Company.class)))
+                .thenReturn(createdCompany);
+        mockMvc.perform(post("/api/companies")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(requestBody))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.name").value("JetBrains"));
+        verify(companyService).createCompany(any(Company.class));
     }
 }
